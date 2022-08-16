00f7685a370a2a8f44d8a4662816ed96ba9ebebe
cirrusflyer <33668552+cirrusflyer@users.noreply.github.com>
2022-08-16 11:22:37 -0400

    Update install-bhyve.sh
    removed GNOME installation and VNC

--- install-bhyve.sh
+++ install-bhyve.sh
@@ -4,7 +4,7 @@
 #
 # This script will install the packages and make the configuration
 # changes necessary to give a freshly-installed FreeBSD 13.1 system
-# a Gnome3 desktop, virtualization support, and the vm-bhyve CLI
+# virtualization support, and the vm-bhyve CLI
 # management system for new VMs.
 #
 # NOTE: script assumes the FreeBSD system was installed to a ZFS root
@@ -13,18 +13,8 @@
 echo Updating package system...
 pkg update
 
-echo Installing GNOME3 desktop environment...
-pkg install -y xorg gdm gnome gnome-desktop
-
 echo Installing vm-bhyve and related tools...
-pkg install -y vm-bhyve bhyve-firmware tigervnc-viewer
-
-echo Configuring /etc/rc.conf for GNOME3 desktop environment...
-sysrc gnome_enable="YES"
-sysrc moused_enable="YES"
-sysrc dbus_enable="YES"
-sysrc hald_enable="YES"
-sysrc gdm_enable="YES"
+pkg install -y vm-bhyve bhyve-firmware
 
 echo Enabling /proc filesystem...
 echo "proc /proc procfs rw 0 0" >> /etc/fstab
@@ -51,10 +41,5 @@ vm switch create public
 vm switch add public $IF
 
 echo Congratulations! Your FreeBSD system should be ready to boot
-echo to a GNOME3 desktop and start running Bhyve guests now.
-echo 
-echo However, you probably still need to manually install the correct
-echo video driver for your system. To find the correct package, look
-echo for your GPU hardware in the output of pkg search xf86-video. Once
-echo you\'ve found the correct driver package, pkg install xf86-video-\<yourcard\>
+echo and start running Bhyve guests now.
 echo and reboot the system with shutdown -r now.
